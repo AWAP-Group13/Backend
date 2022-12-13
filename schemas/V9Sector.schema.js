3320beaecf0adbeb4f3da1c9557b91f3255ba99f
Kweche-Ebechue-Divine <kwecheebechued@gmail.com>
2022-12-13 12:00:11 +0200

    error handled in v9subsector

--- schemas/V9Sector.schema.js
+++ schemas/V9Sector.schema.js
@@ -4,7 +4,7 @@ const { Schema } = mongoose;
 const V9SectorSchema = new Schema({
   Sector: String,
   co2: Number,
-  forSector: String,
+  
 });
 
 const V9Sector = mongoose.model("V9Sector", V9SectorSchema, "V9Sector");
--- schemas/V9SubSector.schema.js
+++ schemas/V9SubSector.schema.js
@@ -4,6 +4,7 @@ const { Schema } = mongoose;
 const V9SubSectorSchema = new Schema({
   sector: String,
   co2: Number,
+  forSector: String,
 });
 
 const V9SubSector = mongoose.model("V9SubSector", V9SubSectorSchema, "V9SubSector");
