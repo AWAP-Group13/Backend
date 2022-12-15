59faa4a9645d33505c70291b7a94700b196e8d56
SanjayKhatiChhetri <sanjaykhati579@gmail.com>
2022-12-15 17:54:11 +0545

    fix v8 error

--- controllers/stats.controller.js
+++ controllers/stats.controller.js
@@ -10,10 +10,9 @@ const Vostok_co2_Data = require("../schemas/vostok_co2_data_v5.schema");
 const V6 = require("../schemas/v6.schema");
 const surfaceTempV7 = require("../schemas/surfaceTempV7.schema");
 const co2DataV7 = require("../schemas/co2DataV7.schema");
-const V8 = require("../schemas/v8.schema");
+const V8 = require("../schemas/V8.schema");
 const V9Sector = require("../schemas/v9Sector.schema");
 const V9SubSector = require("../schemas/v9SubSector.schema");
-
 
 async function getV1MonthlyStats(req, res) {
   try {
